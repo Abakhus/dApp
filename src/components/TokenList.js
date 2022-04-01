f22df24e61cd5c323aad148f4209f37f32d786ec
amrm121 <amrm@cin.ufpe.br>
2022-04-01 14:08:52 -0300

    Creating forms to mint NFT

--- src/components/TokenList.js
+++ src/components/TokenList.js
@@ -6,16 +6,9 @@ function TokenList({ nftList }) {
         return nftList.map((item,key) => {
             return (
                 <div key={key}>
-                    <div>
-                    {item.image 
-	                    ? <img src={item.image} alt={item.image}></img> 
-	                    : <img 
-		                    src="https://i.picsum.photos/id/551/200/300.jpg?hmac=pXJCWIikY_BiqwhtawBb8x1jxclDny0522ZprZVTJiU" 
-		                    alt="default">
-		                   </img>
-		              }
-		             </div>
+                    <div>{item.image ? <img src={item.image} alt={item.image}></img> : <img src="https://i.picsum.photos/id/551/200/300.jpg?hmac=pXJCWIikY_BiqwhtawBb8x1jxclDny0522ZprZVTJiU" alt="default"></img>}</div>
                     <div>{item.name}</div>
+                    <div>{item.description}</div>
                 </div>
             );
         });
--- src/contracts/labReport.js
+++ src/contracts/labReport.js
@@ -4,19 +4,8 @@ import {
     extendContract
 } from '@stakeordie/griptape.js';
 
-const abkt_permit = {
-    queries: {
-      getBalance({ permit }) {
-        const query = { all_tokens: {} }
-        return {
-          with_permit: { query, permit }
-        }
-      }
-    }
-  }
-
 export const abkt = createContractClient({
     id: 'abkt',
     at: 'secret1kj308k8t4g4d0vhs3cg9xvd8ykam6s4yn8xss9',
-    definition: extendContract(snip721Def, abkt_permit)
+    definition: snip721Def
 }); //At definition we can use extendContract(snip721Def, customDef)  - So we can use a single contract definiton as a product of binding both
