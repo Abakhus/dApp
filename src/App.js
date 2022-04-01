6e61fc5a7e47ab008e7742c669cb77e378651d8f
amrm121 <amrm@cin.ufpe.br>
2022-04-01 02:23:59 -0300

    Data flow control

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ function App() {
   var [isConnected, setIsConnected] = useState(false);
   var [tokens, setTokens] = useState([]);
   var [isAccountChanged, setIsAccountChanged] = useState(false);
+  var [haveViewKey, setViewKey] = useState(false);
+  var [viewingKey, setViewingKey] = useState('');
+  var [loading, setLoading] = useState(false);
+
 
 
   useEffect(() => {
@@ -35,7 +39,7 @@ function App() {
     */
     const removeAccountAvailableListener = onAccountAvailable (() =>{
       setIsConnected(true);
-      const key = viewingKeyManager.get(abkt.at);
+      const key = viewingKeyManager.get(abkt.at); //Get actual viewing key
       if(key){
         setViewingKey(key);
         getBalance();
@@ -53,13 +57,38 @@ function App() {
     }//*/
   }, []);
 
+  const createViewingKey = async () =>{
 
+    setLoading(true);
+    try{
+      const result = await abkt.createViewingKey();
+      
+      if (result.isEmpty()) return;
 
+      const { create_viewing_key: { key } } = result.parse();
+      viewingKeyManager.add(abkt, key);
+      setViewingKey(key);
+      const currentKey = viewingKeyManager.get(abkt.at);
+
+      if(currentKey) {
+        viewingKeyManager.set(abkt, key);
+      } else {
+        viewingKeyManager.add(abkt, key);
+      }
+    
+    } catch (e) {
+
+    } finally {
+      setLoading(false);
+    }
+  }
 
   const getBalance = async () => {
-    var balance = await getNativeCoinBalance();
-    balance = coinConvert(balance, 6, 'human');
-    setCoins(balance);
+   const key = viewingKeyManager.get(abkt.at);
+   if (!key) return;
+   const amount = await abkt.getBalance();
+   const balance = coinConvert(amount.balance.amount, 6, 'human');
+   setCoins(balance);
   }
 
   return (
@@ -67,6 +96,7 @@ function App() {
 
       <h1>Hello, Griptape!</h1>
       <Componente size={"20"}/>
+      <p>Viewing Key exists? {haveViewKey ? "Yes" : "No"}</p>
       <p>Is connected? {isConnected ? "Yes" : "No"}</p>
       <button
         onClick={() => { bootstrap(); }}
@@ -75,6 +105,9 @@ function App() {
       <p>Your address is: {address}</p>
       <p>Your balance is: {coins}</p>
       <p>Your minted tokens:</p>
+      <p>Your balance is: {coins}</p>
+      <button disabled={!isConnected} onClick={() => { createViewingKey(); }}>{loading ? 'Loading...' : 'Create Viewing Key'}</button>
+      <button hidden={isAccountChanged} onClick={() => { window.location.reload(); }}>Refresh</button>
     </>
   );
 }
