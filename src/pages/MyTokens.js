3db6de50459105e36f3152c1d5c9bb4049f989b4
amrm121 <amrm@cin.ufpe.br>
2022-04-13 03:30:29 -0300

    getting tokens

--- src/pages/MyTokens.js
+++ src/pages/MyTokens.js
@@ -1,9 +1,53 @@
-import React, { useState } from 'react';
-import Card from 'react-bootstrap/Card'
-import "./Box.css";
-import { CCard, CCardImage, CCardBody, CCardTitle, CCardText, CButton } from '@coreui/react'
+import React, { useState, useEffect } from 'react';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import CardActions from "@material-ui/core/CardActions";
+import './Box.css'
+import { abkt } from './contracts/labReport';
+
+// return minting.getNftDossier(token,false,true);  retornar os dados da token por completo
+
 
 const MyTokens = () => {
+
+var [loadingTokens, setLoadingTokens] = useState(false);
+var [tokens, setTokens] = useState([]);
+
+const getNftDetail = async (token_list) => {
+	const promises = token_list.map(token => {
+	  //Query each token 
+	  return abkt.getNftDossier(token,false,true); //
+	});
+	
+	const result = await Promise.all(promises);
+	console.log(result);
+	// console.log('result: ', result);
+	this.tokens = result.map((ele, idx) => {
+	  ele['nft_dossier']['index'] = `${token_list[idx]}`;
+	  return ele['nft_dossier'];
+	});
+  }
+
+const getTokens = async () =>{
+	setLoadingTokens(true);
+		try {
+		//Get list of tokens' id owned
+		// Exam. ["4","65","87"]
+		const tokens = await abkt.getTokens(null,null,10,true);
+		const token_list = tokens.token_list.tokens;
+		console.log(tokens);
+		//Get details of each token
+		await this.getNftDetail(token_list);
+	} catch (e) {
+		console.error(e)
+	} finally {
+		setLoadingTokens(false);
+	}
+}
+	useEffect(() => {
+	  }, []);
 	const nftInfo = [
 		{
 			file : "",
@@ -31,21 +75,54 @@ const MyTokens = () => {
 			text: "Sample txt 2",
 		  },
 	]
+
+
 	
-	const renderCard = (card, index) => {
-		
-		return (		
-		<CCard style={{ width: '18rem' }}>
-			<CCardImage orientation="top" src="/images/react.jpg" />
-			<CCardBody>
-				<CCardTitle>Card title</CCardTitle>
-				<CCardText>
-				Some quick example text to build on the card title and make up the bulk of the card's content.
-				</CCardText>
-				<CButton href="#">Go somewhere</CButton>
-			</CCardBody>
-		</CCard>
-		)
+
+	return (
+		<div style={{
+			justifyContent: 'Center',
+			alignItems: 'Center',
+		}}>
+		  <h4>How to use CardComponent in ReactJS?</h4>
+		  <Card
+			style={{
+			  width: 400,
+			  backgroundColor: "yellow",
+			}}
+		  >
+			<CardContent>
+			  <Typography
+				style={{ fontSize: 14 }}
+				color="textSecondary"
+				gutterBottom
+			  >
+				Greetings of the day
+			  </Typography>
+			  <Typography variant="h5" component="h2">
+				How are you ?
+			  </Typography>
+			  <Typography
+				style={{
+				  marginBottom: 12,
+				}}
+				color="textSecondary"
+			  >
+				Keep Motivated
+			  </Typography>
+			  <Typography variant="body2" component="p">
+				Stay Happy
+			  </Typography>
+			</CardContent>
+			<CardActions>
+			  <Button 
+			  onClick={() => { getTokens(); } } 
+			  size="small">Stay Safe.....</Button>
+			</CardActions>
+		  </Card>
+		</div>
+	  );
+	
 		/*return (
 			<Card style={{ width: "18rem" }} key={index} className="box">
 			<Card.Img variant="top" src="holder.js/100px180" src={card.image} />
@@ -55,11 +132,6 @@ const MyTokens = () => {
 			</Card.Body>
 		  </Card>
 		);*/
-	  };
-	
-	  return (		
-		<div>{cardT.map(renderCard)}</div>	
-	);
 	  
 };
 
