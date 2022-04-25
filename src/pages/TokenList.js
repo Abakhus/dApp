620360f16234c5bd1e6c23b9ae70898297857304
amrm121 <amrm@cin.ufpe.br>
2022-04-25 10:28:42 -0300

    fixed my tokens view grid

--- src/pages/TokenList.js
+++ src/pages/TokenList.js
@@ -4,7 +4,7 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import PopupPData from "./PopupPData";
+
 
 function TokenList({ nftList }) {
 
