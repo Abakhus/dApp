0a973d1a4c2539e245fdb33482066a82e33eaf01
amrm121 <amrm@cin.ufpe.br>
2022-05-01 19:01:27 -0300

    Added the genomic report tokenize page

---
+++ src/pages/TokenizeAnalisys.js
@@ -0,0 +1,183 @@
+/* 
+Requisitos:
+    • Metadados públicos:
+        ◦ Código da empresa de análise.
+        ◦ Código do relatório (não será o nome do exame, mas um código interno da empresa de análise).
+        ◦ Data da liberação do relatório (não é, obrigatoriamente, a data da criação do token).
+        ◦ URL para a imagem de capa do relatório, caso exista.
+    • Metadados privados:
+        ◦ URL do relatório (pdf).
+
+Token para disponibilizar o relatório de análises genômicas
+*/
+import { 
+  onAccountAvailable,
+  viewingKeyManager,
+  } from '@stakeordie/griptape.js';
+import { useState } from "react";
+import './Popup.css'
+
+//importando o contrato específico pra o mint da token
+import { genomic } from './contracts/AnalisysReport'
+
+const TokenizeGenomicAnalisys = () => {
+    //misc 
+    var [loadingMint, setLoadingMint] = useState(false);
+    var [isConnected, setIsConnected] = useState(false);
+    const ipfsClient = create('https://ipfs.infura.io:5001/api/v0');
+    var [fileStatus, setFileStatus] = useState(false);
+    
+    //public
+    var [bgImage, setBgImage] = useState(``);
+    var [labID, setLabID] = useState('');
+    var [reportID, setReportID] = useState('');
+    var [releaseDate, setReleaseDate] = useState('');
+    //private
+    var [fileURL, setFileURL] = useState(``);
+
+    useEffect(() =>{
+      const removeOnAccountAvailable = onAccountAvailable (() => {
+          setIsConnected(true);
+          const key = viewingKeyManager.get(genomic.at);
+          if(key){
+            setViewingKey(key);
+          }
+        })
+        return () => {
+          removeOnAccountAvailable();
+        }
+  }, []);
+
+    //token minting function 
+    const mint = async () => {
+        var date = Date.now(); 
+        const extension = {
+          //public metadata
+            name: "",
+            description: "",
+            image: "",
+            attributes: [
+            {
+              "trait_type": "report_id",
+              "value": `${reportID}`
+            },
+            {
+              "trait_type": "lab_id",
+              "value": `${labID}`
+            },
+            {
+              "trait_type": "release_date",
+              "value": `${releaseDate}`
+            } 
+         ] 
+        }
+        //private metadata
+        const private_metadata = {
+          extension : {
+            attributes: [{              
+              "trait_type": "file",
+              "value": `${fileURL}`
+            }]
+          }
+        }
+        //minting 
+        setLoadingMint(true);
+        try {
+          await genomic.mintNft(null,null,{extension}, private_metadata );
+        } catch (e) {
+          //how to behave when minting fails?
+        } finally {
+          setLoadingMint(false);
+        }
+    }
+
+    //function to upload a file to IPFS
+    async function ipfsUpload(e) {
+      const file = e.target.files[0]
+      try {
+        const added = await ipfsClient.add(file)
+        const url = `https://ipfs.infura.io/ipfs/${added.path}`
+        //setFile(url);
+        console.log(url);
+        setFileURL(url);
+      } catch (error) {
+        console.log('Error uploading file: ', error)
+      }  finally {
+        setFileStatus(true);
+      }
+    }
+
+    return (
+      <>      
+      <div className='container' >
+        <strong>Mint Genomic Analisys Token</strong>
+        <hr></hr>
+      <form>
+        <fieldset>
+          <p>Public Metadata:</p>
+        <form>
+        <label>Laboratory ID:   
+          <input
+            type="text" 
+            value={labID}
+            onChange={(e) => setLabID(e.target.value)}
+          />
+        </label>
+      </form>
+      <form>
+        <label>Report ID:  
+          <input
+            type="text" 
+            value={reportID}
+            onChange={(e) => setReportID(e.target.value)}
+          />
+        </label>
+      </form>
+      <form>
+        <label>Release Date:  
+          <input
+            type="text" 
+            value={releaseDate}
+            onChange={(e) => setReleaseDate(e.target.value)}
+          />
+        </label>
+      </form>
+      {/* <form>
+        <label>Image URL:  
+          <input
+            type="text" 
+            value={fileURL}
+            onChange={(e) => setFileURL(e.target.value)}
+          />
+        </label>
+      </form> */}
+      </fieldset>
+    </form>
+    <form>
+      <fieldset>
+        <p>Private Metadata: </p>
+      <form>
+        <label>Report File:  
+          <input
+            type="file"
+            onChange={ipfsUpload}
+          />
+        </label>
+      </form>
+      </fieldset>
+    </form>
+    <br></br>
+    <p>
+    <Button variant="contained" onClick={ () => { mint(); } } disabled={ !fileStatus } className='cta-button mint-nft-button'>
+      {loadingMint ? 'Minting...' : 'Mint'}
+    </Button>
+    <br></br>
+    {!fileStatus && 
+    "Upload a file to mint!" } 
+    </p>
+    </div>
+    </>
+  )
+
+}
+export default TokenizeGenomicAnalisys;
--- src/pages/contracts/AnalisysReport.js
+++ src/pages/contracts/AnalisysReport.js
@@ -3,8 +3,8 @@ import {
     snip721Def
 } from '@stakeordie/griptape.js';
 
-export const abkt  = createContractClient({
-    id: 'abkt',
+export const genomic = createContractClient({
+    id: 'GenomicReport',
     at: process.env.REACT_APP_ANALISYS_CONT,
     definition: snip721Def
 }); //At definition we can use extendContract(snip721Def, customDef)  - So we can use a single contract definiton as a product of binding both
